dff6ba7128c84d9d3eea88633f5328d19f3eb1be
Mohammed Niyas <Mohammed Niyas>
2018-01-12 15:08:06 +0000

    added code framework

---
+++ TicketAutomation_Emailer/DAL.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace TicketAutomation_Emailer
+{
+    class DAL
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["OPAS_ETL_Connection"].ConnectionString);
+        private static IDbCommand cmd = new SqlCommand();
+        private string strConnectionString = "";
+        private bool handleErrors = false;
+        private string strLastError = "";
+
+        public DAL()
+        {
+            strConnectionString = ConfigurationManager.ConnectionStrings["OPAS_ETL_Connection"].ConnectionString;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = strConnectionString;
+            cmd.Connection = con;
+            _CommandType = CommandType.StoredProcedure;
+            cmd.CommandType = _CommandType;
+        }
+
+        private CommandType _CommandType;
+
+        public CommandType commandType
+        {
+            get { return _CommandType; }
+            set { _CommandType = value; }
+        }
+
+
+        public IDataReader ExecuteReader()
+        {
+            IDataReader reader = null;
+            try
+            {
+                this.Open();
+                reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+            return reader;
+        }
+
+        public IDataReader ExecuteReader(string commandtext)
+        {
+            IDataReader reader = null;
+            try
+            {
+                cmd.CommandText = commandtext;
+                reader = this.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+
+            return reader;
+        }
+
+        public object ExecuteScalar()
+        {
+            object obj = null;
+            try
+            {
+                this.Open();
+                obj = cmd.ExecuteScalar();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+
+            return obj;
+        }
+
+        public object ExecuteScalar(string commandtext)
+        {
+            object obj = null;
+            try
+            {
+                cmd.CommandText = commandtext;
+                cmd.CommandType = _CommandType;
+                obj = this.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+
+            return obj;
+        }
+
+        public int ExecuteNonQuery()
+        {
+            int i = -1;
+            try
+            {
+                this.Open();
+                i = cmd.ExecuteNonQuery();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+
+            return i;
+        }
+
+
+        public int ExecuteNonQuery(string commandtext)
+        {
+            int i = -1;
+            try
+            {
+                cmd.CommandText = commandtext;
+                cmd.CommandType = _CommandType;
+                i = this.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+
+            return i;
+        }
+
+
+        public DataSet ExecuteDataSet()
+        {
+            SqlDataAdapter da = null;
+            DataSet ds = null;
+            try
+            {
+                cmd.CommandType = _CommandType;
+                da = new SqlDataAdapter();
+                da.SelectCommand = (SqlCommand)cmd;
+                if (da.SelectCommand.Connection.State == ConnectionState.Closed)
+                {
+                    da.SelectCommand.Connection.Open();
+                }
+                ds = new DataSet();
+                da.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+
+
+            return ds;
+        }
+
+
+        public DataSet ExecuteDataSet(string commandtext)
+        {
+            DataSet ds = null;
+            try
+            {
+                cmd.CommandText = commandtext;
+                ds = this.ExecuteDataSet();
+            }
+            catch (Exception ex)
+            {
+                if (handleErrors)
+                    strLastError = ex.Message;
+                else
+                    throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                this.Close();
+            }
+
+            return ds;
+        }
+
+        public string CommandText
+        {
+            get
+            {
+                return cmd.CommandText;
+            }
+            set
+            {
+                cmd.CommandText = value;
+                cmd.Parameters.Clear();
+            }
+        }
+
+        public IDataParameterCollection Parameters
+        {
+            get
+            {
+                return cmd.Parameters;
+            }
+        }
+
+        public void AddParameter(string paramname, object paramvalue)
+        {
+            SqlParameter param = new SqlParameter(paramname, paramvalue);
+            cmd.Parameters.Add(param);
+        }
+
+        public void AddParameter(IDataParameter param)
+        {
+            cmd.Parameters.Add(param);
+        }
+
+
+        public string ConnectionString
+        {
+            get
+            {
+                return strConnectionString;
+            }
+            set
+            {
+                strConnectionString = value;
+            }
+        }
+
+        private void Open()
+        {
+            try
+            {
+                cmd.Connection.Open();
+            }
+            catch { }
+        }
+
+        private void Close()
+        {
+            try
+            {
+                cmd.Connection.Close();
+            }
+            catch { }
+        }
+
+        public bool HandleExceptions
+        {
+            get
+            {
+                return handleErrors;
+            }
+            set
+            {
+                handleErrors = value;
+            }
+        }
+
+        public string LastError
+        {
+            get
+            {
+                return strLastError;
+            }
+        }
+
+        public void Dispose()
+        {
+            cmd.Dispose();
+        }
+
+    }
+}
---
+++ TicketAutomation_Emailer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using System.Net;
+using System.Net.Mail;
+using System.Data;
+using System.Configuration;
+
+
+namespace TicketAutomation_Emailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getReport();
+        }
+
+        private static void getReport()
+        {
+            DAL objDAL = new DAL();
+            DataTable dt = new DataTable();
+            string filePath = ConfigurationManager.AppSettings["folderPath"].ToString() + "WeeklyReport.xlsx";
+            string Query = "usp_get_WeeklyReportData";
+            objDAL.CommandText = Query;
+            dt = objDAL.ExecuteDataSet().Tables[0];
+            XLWorkbook wb = new ClosedXML.Excel.XLWorkbook();
+            wb.Worksheets.Add(dt);
+            wb.SaveAs(filePath);
+         }
+
+        private static void sendEmail(string url, string to, string cc, string name, string emailBody, string attachmentPath)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient();
+                if (attachmentPath.Length > 0)
+                {
+                    System.Net.Mail.Attachment attachment = new System.Net.Mail.Attachment(attachmentPath);
+                    mail.Attachments.Add(attachment);
+                }
+                mail.From = new MailAddress("niyasatwork@gmail.com");
+                mail.To.Add(to);
+                mail.Subject = "Ticket Automation Email";
+                mail.Body = "Hi " + name + ",<br/>" + emailBody;
+                mail.IsBodyHtml = true;
+                SmtpServer.Host = "smtp.gmail.com";
+                SmtpServer.Port = 587;
+                SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("niyasatwork@gmail.com", "yourpassword");
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.EnableSsl = true;
+                SmtpServer.Send(mail);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            
+        }
+    }
+}
